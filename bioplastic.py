0a27034bfefb6ec444c19fbeb0eee33369c856bb
vanCBarE <ibarragan@unal.edu.co>
2023-09-02 08:44:29 -0500

    class bioplastic

---
+++ bioplastic.py
@@ -0,0 +1,8 @@
+class bioplastic_production:
+    def __init__(self, TnMaiz_per_ha:float=3.39, almidon_per_gr_maiz:float=0.057, bioplastic_per_almidon:float=0.8741) -> None:
+        """The default values are for the production with the minimum yield and
+          efficiency reported in the literature."""
+        self.gr_maiz_per_ha = TnMaiz_per_ha * 1000000
+        self.gr_almidon_per_ha = self.gr_maiz_per_ha * almidon_per_gr_maiz
+        self.Tn_bioplastic_per_ha = (self.almidon_per_ha * bioplastic_per_almidon) / 1000000
+        
